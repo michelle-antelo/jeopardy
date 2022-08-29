78fc6b4a1019997df552b4c606ac735fc6794fdc
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-08-29 14:54:59 -0400

    final commit project

---
+++ jeopardy/jeopardy.js
@@ -0,0 +1,84 @@
+const apiUrl = "https://jservice.io/api/";
+const numCat = 6;
+const numQuestionsPerCat = 5;
+
+let categories = [];
+
+async function getCategoryIds() {
+  let response = await axios.get(`${apiUrl}categories?count=100`);
+  let catIds = response.data.map(c => c.id);
+  return _.sampleSize(catIds, numCat);
+}
+
+async function getCategory(catId) {
+  let response = await axios.get(`${apiUrl}category?id=${catId}`);
+  let cat = response.data;
+  let catClues = cat.clues;
+  let randomClues = _.sampleSize(catClues, numQuestionsPerCat);
+  let clues = randomClues.map(c => ({
+    question: c.question,
+    answer: c.answer,
+    showing: null,
+  }));
+
+  return { title: cat.title, clues };
+}
+
+
+async function fillTable() {
+  $("#jeopardy thead").empty();
+  let tr = $("<tr>");
+  for (let catIdx = 0; catIdx < numCat; catIdx++) {
+    tr.append($("<th>").text(categories[catIdx].title));
+  }
+  $("#jeopardy thead").append(tr);
+
+  $("#jeopardy tbody").empty();
+  for (let clueIdx = 0; clueIdx < numQuestionsPerCat; clueIdx++) {
+    let tr = $("<tr>");
+    for (let catIdx = 0; catIdx < numCat; catIdx++) {
+      tr.append($("<td>").attr("id", `${catIdx}-${clueIdx}`).text("?"));
+    }
+    $("#jeopardy tbody").append(tr);
+  }
+}
+
+function handleClick(event) {
+  let id = event.target.id;
+  let [catId, clueId] = id.split("-");
+  let clue = categories[catId].clues[clueId];
+
+  let msg;
+
+  if (!clue.showing) {
+    msg = clue.question;
+    clue.showing = "question";
+  } else if (clue.showing === "question") {
+    msg = clue.answer;
+    clue.showing = "answer";
+  } else {
+    return
+  }
+
+  $(`#${catId}-${clueId}`).html(msg);
+}
+
+async function setupAndStart() {
+  let catIds = await getCategoryIds();
+
+  categories = [];
+
+  for (let catId of catIds) {
+    categories.push(await getCategory(catId));
+  }
+
+  fillTable();
+}
+
+$("#restart").on("click", setupAndStart);
+
+$(async function () {
+    setupAndStart();
+    $("#jeopardy").on("click", "td", handleClick);
+  }
+);
